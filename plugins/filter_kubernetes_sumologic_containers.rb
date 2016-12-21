bf7aa7064aaab45336294d4240e01c3c857dc685
Steven Adams <stevezau@gmail.com>
2016-12-21 21:47:08 +1100

    handle overriding metadata fields at config level. fixes #1

--- plugins/filter_kubernetes_sumologic_containers.rb
+++ plugins/filter_kubernetes_sumologic_containers.rb
@@ -10,6 +10,8 @@ module Fluent
     config_param :source_category_replace_dash, :string, :default => '/'
     config_param :source_category_prefix, :string, :default => 'kubernetes/'
     config_param :source_name, :string, :default => '%{namespace}.%{pod}.%{container}'
+    config_param :log_format, :string, :default => 'json'
+    config_param :source_host, :string, :default => nil
 
     def configure(conf)
       super
@@ -20,12 +22,21 @@ module Fluent
     end
 
     def filter(tag, time, record)
-      unless record.fetch('kubernetes').nil?
+      # Set the sumo metadata fields
+      sumo_metadata = record[:_sumo_metadata] = {}
+      sumo_metadata[:log_format] = @log_format
+      sumo_metadata[:host] = metadata[:source_host] if @source_host
+      sumo_metadata[:source] = @source_name if @source_name
 
+      unless @source_category.nil?
+        sumo_metadata[:category] = @source_category.prepend(@source_category_prefix)
+      end
+
+      # Allow fields to be overridden by annotations
+      unless record.fetch('kubernetes').nil?
         # Clone kubernetes hash so we don't override the cache
         kubernetes = record['kubernetes'].clone
-
-        metadata = {
+        k8s_metadata = {
             :namespace => kubernetes['namespace_name'],
             :pod => kubernetes['pod_name'],
             :container => kubernetes['container_name'],
@@ -36,19 +47,21 @@ module Fluent
         # NOTE: Kubernetes deployments append a template hash.
         pod_parts = metadata[:pod].split('-')
         if is_number?(pod_parts[-2])
-          metadata[:pod_name] = pod_parts[0..-3].join('-')
+          k8s_metadata[:pod_name] = pod_parts[0..-3].join('-')
         else
-          metadata[:pod_name] = pod_parts[0..-2].join('-')
+          k8s_metadata[:pod_name] = pod_parts[0..-2].join('-')
         end
 
         annotations = kubernetes.fetch('annotations', {})
 
-        sumo_metadata = record[:_sumo_metadata] = {}
-        sumo_metadata[:host] = metadata[:source_host]
-        sumo_metadata[:source] = (annotations['sumologic.com/sourceName'] || @source_name) % metadata
-        sumo_metadata[:category] = ((annotations['sumologic.com/sourceCategory'] || @source_category) % metadata).prepend(@source_category_prefix)
-        sumo_metadata[:category].gsub!('-', @source_category_replace_dash)
-        sumo_metadata[:log_format] = annotations['sumologic.com/format']
+        sumo_metadata[:log_format] = annotations['sumologic.com/format'] if annotations['sumologic.com/format']
+        sumo_metadata[:host] = k8s_metadata[:source_host]
+        sumo_metadata[:source] = annotations['sumologic.com/sourceName'] % k8s_metadata if annotations['sumologic.com/sourceName']
+
+        if annotations['sumologic.com/sourceCategory'].nil?
+          sumo_metadata[:category] = annotations['sumologic.com/sourceCategory'] % k8s_metadata.prepend(@source_category_prefix)
+          sumo_metadata[:category].gsub!('-', @source_category_replace_dash)
+        end
 
         # Strip kubernetes metadata from json if disabled
         if annotations['sumologic.com/kubernetes_meta'] == 'false' || !@kubernetes_meta
