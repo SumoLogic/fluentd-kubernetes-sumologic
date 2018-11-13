7ff0526fd842e4b6463992e9259ce160c39cf7a8
Aditya Patel <adipatel@users.noreply.github.com>
2018-11-13 16:11:32 +0530

    SUMO-98617: Add test for fields format

--- test/plugin/test_filter_kubernetes_sumologic.rb
+++ test/plugin/test_filter_kubernetes_sumologic.rb
@@ -83,6 +83,52 @@ class SumoContainerOutputTest < Test::Unit::TestCase
     assert_equal(d.filtered_records[0], expected)
   end
 
+  test "test_fields_format" do
+    conf = %{
+      log_format fields
+    }
+    d = create_driver(conf)
+    time = @time
+    input = {
+      "timestamp" => 1538677347823,
+      "log" => "some message",
+      "stream" => "stdout",
+      "docker" => {
+        "container_id" => "5c280b6ad5abec32e9af729295c20f60fbeadf3ba16fda2d121f87228e6822e0",
+      },
+      "kubernetes" => {
+        "container_name" => "log-format-labs",
+        "namespace_name" => "default",
+        "pod_name" => "log-format-labs-54575ccdb9-9d677",
+        "pod_id" => "170af806-c801-11e8-9009-025000000001",
+        "labels" => {
+          "pod-template-hash" => "1013177865",
+          "run" => "log-format-labs",
+        },
+        "host" => "docker-for-desktop",
+        "master_url" => "https =>//10.96.0.1 =>443/api",
+        "namespace_id" => "e8572415-9596-11e8-b28b-025000000001",
+      },
+    }
+    d.run do
+      d.feed("filter.test", time, input)
+    end
+    expected = {
+      "timestamp" => 1538677347823,
+      "log" => "some message",
+      "stream" => "stdout",
+      "_sumo_metadata" => {
+        :category => "kubernetes/default/log/format/labs/54575ccdb9",
+        :host => "",
+        :log_format => "fields",
+        :source => "default.log-format-labs-54575ccdb9-9d677.log-format-labs",
+        :fields => "container_id=5c280b6ad5abec32e9af729295c20f60fbeadf3ba16fda2d121f87228e6822e0,namespace=default,pod=log-format-labs-54575ccdb9-9d677,container=log-format-labs,source_host=docker-for-desktop,label:pod-template-hash=1013177865,label:run=log-format-labs,pod_name=log-format-labs-54575ccdb9",
+      },
+    }
+    assert_equal(1, d.filtered_records.size)
+    assert_equal(d.filtered_records[0], expected)
+  end
+
   test "test_no_k8s_labels" do
     conf = %{}
     d = create_driver(conf)
