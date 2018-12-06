2df5ac9255fdcf494fbd6cfdf9e8fb76718cf0f7
pawelbojanowski <pawel.bojanowski@saucelabs.com>
2018-12-06 09:29:26 +0100

    Add test for add_time config param

--- test/plugin/test_filter_kubernetes_sumologic.rb
+++ test/plugin/test_filter_kubernetes_sumologic.rb
@@ -253,6 +253,68 @@ class SumoContainerOutputTest < Test::Unit::TestCase
     assert_equal(d.filtered_records[0], expected)
   end
 
+  test "test_add_time" do
+    conf = %{
+      add_time false
+    }
+    d = create_driver(conf)
+    time = @time
+    input = {
+        "timestamp" => 1538677347823,
+        "log" => "some message",
+        "stream" => "stdout",
+        "time" => time,
+        "docker" => {
+            "container_id" => "5c280b6ad5abec32e9af729295c20f60fbeadf3ba16fda2d121f87228e6822e0",
+        },
+        "kubernetes" => {
+            "container_name" => "log-format-labs",
+            "namespace_name" => "default",
+            "pod_name" => "log-format-labs-54575ccdb9-9d677",
+            "pod_id" => "170af806-c801-11e8-9009-025000000001",
+            "labels" => {
+                "pod-template-hash" => "1013177865",
+                "run" => "log-format-labs",
+            },
+            "host" => "docker-for-desktop",
+            "master_url" => "https =>//10.96.0.1 =>443/api",
+            "namespace_id" => "e8572415-9596-11e8-b28b-025000000001",
+        },
+    }
+    d.run do
+      d.feed("filter.test", time, input)
+    end
+    expected = {
+        "timestamp" => 1538677347823,
+        "log" => "some message",
+        "stream" => "stdout",
+        "docker" => {
+            "container_id" => "5c280b6ad5abec32e9af729295c20f60fbeadf3ba16fda2d121f87228e6822e0",
+        },
+        "kubernetes" => {
+            "container_name" => "log-format-labs",
+            "namespace_name" => "default",
+            "pod_name" => "log-format-labs-54575ccdb9-9d677",
+            "pod_id" => "170af806-c801-11e8-9009-025000000001",
+            "labels" => {
+                "pod-template-hash" => "1013177865",
+                "run" => "log-format-labs",
+            },
+            "host" => "docker-for-desktop",
+            "master_url" => "https =>//10.96.0.1 =>443/api",
+            "namespace_id" => "e8572415-9596-11e8-b28b-025000000001",
+        },
+        "_sumo_metadata" => {
+            :category => "kubernetes/default/log/format/labs/54575ccdb9",
+            :host => "",
+            :log_format => "json",
+            :source => "default.log-format-labs-54575ccdb9-9d677.log-format-labs",
+        },
+    }
+    assert_equal(1, d.filtered_records.size)
+    assert_equal(d.filtered_records[0], expected)
+  end
+
   test "test_sourcecategory_replace_dash" do
     conf = %{
       source_category_replace_dash -
